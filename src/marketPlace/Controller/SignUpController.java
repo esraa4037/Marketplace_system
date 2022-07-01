06c93ac2dbd12d1a8bd8efb37a14f73b90b65490
eman877 <95876089+eman877@users.noreply.github.com>
2022-07-01 22:05:48 +0200

    Update SignUpController.java

--- src/marketPlace/Controller/SignUpController.java
+++ src/marketPlace/Controller/SignUpController.java
@@ -3,21 +3,31 @@ package marketPlace.Controller;
 
 import marketPlace.Model.ClientAccount;
 import marketPlace.Model.ServerAccount;
-
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 public class SignUpController {
-    public boolean isClient(ClientAccount account) {
-		if (account.getUserName().equals("esraa") && account.getPassword().equals("1234")) {
-			return false;
-		}
-		return true;
-	}
-	
-	public boolean isServer(ServerAccount account) {
-		if (account.getUserName().equals("admin") && account.getPassword().equals("1234")) {
-			return false;
-		}
-		return true;
-	}
+    Statement z;
+    ConnectionDB ll=new ConnectionDB();
+    Statement o;
+    ConnectionDB pp=new ConnectionDB();
+    public boolean isclient(ClientAccount acc) throws SQLException{
+    o=pp.openconnection().createStatement();
+               
+	ResultSet sss=o.executeQuery("select * from client where Username='"+acc.getUserName()+"'");
+           
+                if(sss.next())
+                    return false;
+	return true;
+    
+    }
+    
+  public void create_client(ClientAccount acc) throws SQLException{
+  z=ll.openconnection().createStatement();
+      z.execute("insert into client(ClientID,Fname,Lname,Email,Phone,PostalCode,UserName,Pass) values(null,'"+acc.getFirstName()+"','"+acc.getLastName()+"','"+acc.getEmail()+"','"+acc.getPhone()+"','"+acc.getPostalCode()+"','"+acc.getUserName()+"','"+acc.getPassword()+"')");
+  
+  }
 	
     
 }
+
