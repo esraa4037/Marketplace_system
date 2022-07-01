1a972d12e30c4f0c6722fde578737ca7ca897c53
alaamhassan <67596481+alaamhassan@users.noreply.github.com>
2022-07-01 23:01:15 +0200

    Update Main.java

--- src/marketPlace/Main.java
+++ src/marketPlace/Main.java
@@ -1,28 +1,60 @@
-package marketPlace;
-	
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
-public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			Parent root = FXMLLoader.load(getClass().getResource("/marketPlace/View/Login.fxml"));
-			Scene scene = new Scene(root);
-//			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setTitle("Market Place");
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+import server_supermarket.ServerThread;
+
+public class Servermain {
+    
+    public static void main(String[] args){
+        
+        ServerSocket serverSocket =null;
+        List<PrintWriter> listWriters = new ArrayList<PrintWriter>();
+
+        try{
+            
+           serverSocket = new ServerSocket();
+           String hostAddress = InetAddress.getLocalHost().getHostAddress();
+           serverSocket.bind(new InetSocketAddress(hostAddress,8080));
+           System.out.println("waiting..");
+           
+           while(true){
+               Socket socket =serverSocket.accept();
+               System.out.println("Client IP: "+socket.getInetAddress());
+               
+               new ServerThread(socket ,listWriters).start();
+               
+           }
+            
+            
+        } catch (IOException ex) {
+            Logger.getLogger(Servermain.class.getName()).log(Level.SEVERE, null, ex);
+        }finally{
+            
+            try{
+                if(serverSocket !=null && !serverSocket.isClosed()){
+                    serverSocket.close();
+                }
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+        }
+        
+    }
+    
 }
