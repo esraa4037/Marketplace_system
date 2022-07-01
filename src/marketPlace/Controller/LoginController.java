204c40477a6de92721a4c52fbb89b7d04a3a5bc4
eman877 <95876089+eman877@users.noreply.github.com>
2022-07-01 22:04:30 +0200

    Update LoginController.java

--- src/marketPlace/Controller/LoginController.java
+++ src/marketPlace/Controller/LoginController.java
@@ -1,21 +1,34 @@
 package marketPlace.Controller;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import marketPlace.Model.ClientAccount;
-import marketPlace.Model.ServerAccount;
 
 public class LoginController {
+	Statement st;
+        ConnectionDB g=new ConnectionDB();
+        Statement sta;
+        ConnectionDB aa=new ConnectionDB() ;
+        //ResultSet r;
+	public boolean isClient(ClientAccount account) throws SQLException {
+		st=g.openconnection().createStatement();
+               
+	ResultSet res=st.executeQuery("select * from client where Username='"+account.getUserName()+"'and pass='"+account.getPassword()+"'");
+           
+                if(res.next())
+                    return true;
+	return false;
+        }
 	
-	public boolean isClient(ClientAccount account) {
-		if (account.getUserName().equals("esraa") && account.getPassword().equals("1234")) {
-			return true;
-		}
-		return false;
-	}
+	public boolean isServer(ClientAccount account)throws SQLException {
+		
+
+		sta=aa.openconnection().createStatement();
+		ResultSet resu=sta.executeQuery("select * from server where username='"+account.getUserName()+"'and pass='"+account.getPassword()+"'");
+                if(resu.next())
+                    return true;
+	return false;
 	
-	public boolean isServer(ServerAccount account) {
-		if (account.getUserName().equals("admin") && account.getPassword().equals("1234")) {
-			return true;
-		}
-		return false;
-	}
-	
+}
 }
