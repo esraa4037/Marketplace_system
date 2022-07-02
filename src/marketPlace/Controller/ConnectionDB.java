3a47b27a239d26e9ee13a6218cde46436c0a2270
NadaSulaiman01 <85398738+NadaSulaiman01@users.noreply.github.com>
2022-07-02 09:06:53 +0200

    Update ConnectionDB.java

--- src/marketPlace/Controller/ConnectionDB.java
+++ src/marketPlace/Controller/ConnectionDB.java
@@ -1,26 +1,9 @@
 package marketPlace.Controller;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 
 public class ConnectionDB {
-Connection con=null;
 
-   public  Connection openconnection()throws SQLException{
-       
-   if(con==null)
-      try {
-          Class.forName("com.mysql.cj.jdbc.Driver");
-            con=DriverManager.getConnection("jdbc:mysql://localhost:3306/ya","root","Hager_Rafaat123");
-   } catch (ClassNotFoundException ex) {
-       Logger.getLogger(ConnectionDB.class.getName()).log(Level.SEVERE, null, ex);
-   }
-   //con=DriverManager.getConnection("jdbc:mysql://localhost:3306/ahah","root","Hager_Rafaat123");
-   return con;
-   }
 
 
 }
