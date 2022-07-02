36d8a523efb7aef76a733ef88f9469667bc84c1b
NadaSulaiman01 <85398738+NadaSulaiman01@users.noreply.github.com>
2022-07-02 09:01:32 +0200

    Update SignUpController.java

--- src/marketPlace/Controller/SignUpController.java
+++ src/marketPlace/Controller/SignUpController.java
@@ -1,32 +1,59 @@
-
 package marketPlace.Controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import marketPlace.Model.ClientAccount;
 import marketPlace.Model.ServerAccount;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import static marketPlace.MyInfo.socket;
+
 public class SignUpController {
-    Statement z;
-    ConnectionDB ll=new ConnectionDB();
-    Statement o;
-    ConnectionDB pp=new ConnectionDB();
-    public boolean isclient(ClientAccount acc) throws SQLException{
-    o=pp.openconnection().createStatement();
-               
-	ResultSet sss=o.executeQuery("select * from client where Username='"+acc.getUserName()+"'");
-           
-                if(sss.next())
-                    return false;
-	return true;
-    
-    }
-    
-  public void create_client(ClientAccount acc) throws SQLException{
-  z=ll.openconnection().createStatement();
-      z.execute("insert into client(ClientID,Fname,Lname,Email,Phone,PostalCode,UserName,Pass) values(null,'"+acc.getFirstName()+"','"+acc.getLastName()+"','"+acc.getEmail()+"','"+acc.getPhone()+"','"+acc.getPostalCode()+"','"+acc.getUserName()+"','"+acc.getPassword()+"')");
-  
-  }
+    public boolean isClient(ClientAccount account) throws IOException {
+
+                String user = account.getUserName();
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                String request = "signupclient";
+                String data = request + ":" + user;
+                System.out.println(data);
+                out.println(data);
+                String serverResponse = input.readLine();
+                System.out.println("serverResponse is:" + serverResponse );
+                 if ("0".equals(serverResponse))
+                {
+                    System.out.println("User name already exists, try again.");
+			return true;
+		}
+                else 
+                {
+                  return false;
+                }                                     
+                              
+	}
+	
+	public void create_client(ClientAccount account) throws IOException {
+
+                 String fname = account.getFirstName();
+                 String lname = account.getLastName();
+                 String email = account.getEmail();
+                 String phone = account.getPhone();
+                 String postal = String.valueOf(account.getPostalCode());
+                 String username = account.getUserName();
+                 String password = account.getPassword();
+          
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+//                BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                String request = "createaccount";
+                System.out.println("about to send data.");
+                System.out.println(postal);
+                System.out.println(account.getPostalCode());
+                
+                String data = request + ":" + fname + ":" + lname + ":" + email + ":" + phone + ":" + postal + ":" + username + ":" + password ;
+                System.out.println(data);
+                out.println(data);
+//             
+	}
 	
     
 }
