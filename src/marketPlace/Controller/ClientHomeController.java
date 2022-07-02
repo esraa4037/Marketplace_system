c84b41240863372e164bb47e0199c3d9249b84b7
Hager44 <68407995+Hager44@users.noreply.github.com>
2022-07-02 06:14:18 +0200

    Update ClientHomeController.java

--- src/marketPlace/Controller/ClientHomeController.java
+++ src/marketPlace/Controller/ClientHomeController.java
@@ -1,14 +1,34 @@
 package marketPlace.Controller;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import marketPlace.Model.ClientAccount;
 
 public class ClientHomeController {
-	public String getFirst(ClientAccount acc) {
-		acc.setFirstName("Esraa");
-		return acc.getFirstName();
+    Statement s;
+    ConnectionDB gh=new ConnectionDB();
+    String h;
+    String e;
+     Statement t;
+      ConnectionDB w=new ConnectionDB();
+	public String getFirst(ClientAccount acc) throws SQLException {
+            s=gh.openconnection().createStatement();
+		ResultSet res=s.executeQuery("select c.Fname from client as c where c.UserName='"+acc.getUserName()+"'" );
+                while(res.next()){
+                h=res.getString("Fname");
+                }
+	return h;
+        }
+	public String getLast(ClientAccount acc) throws SQLException {
+            t=w.openconnection().createStatement();
+		ResultSet res=t.executeQuery("select c.Lname from client as c where c.UserName='"+acc.getUserName()+"'" );
+                while(res.next()){
+                e=res.getString("Lname");
+                }
+	return e;
 	}
-	public String getLast(ClientAccount acc) {
-		acc.setLastName("Mohamed");
-		return acc.getLastName();
-	}
+
+
+       
 }
