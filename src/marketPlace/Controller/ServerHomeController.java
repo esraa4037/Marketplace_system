b4c51a97a88fa6fc6d2e58aa452c7816a793b3b9
eman877 <95876089+eman877@users.noreply.github.com>
2022-07-02 05:59:31 +0200

    Update ServerHomeController.java

--- src/marketPlace/Controller/ServerHomeController.java
+++ src/marketPlace/Controller/ServerHomeController.java
@@ -1,16 +1,32 @@
 package marketPlace.Controller;
 
 import marketPlace.Model.ServerAccount;
-
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 public class ServerHomeController {
-	public String getServerFirstName(ServerAccount saccount) {
-		saccount.setFirstName("Maram");
-		String fname = saccount.getFirstName();
-		return fname;
+     Statement s;
+    ConnectionDB gh=new ConnectionDB();
+    String h;
+    String e;
+     Statement t;
+      ConnectionDB w=new ConnectionDB();
+	public String getServerFirstName(ServerAccount saccount) throws SQLException {
+		 s=gh.openconnection().createStatement();
+		ResultSet res=s.executeQuery("select s.Fname from server as s where username='"+saccount.getUserName()+"'" );
+                while(res.next()){
+                h=res.getString("Fname");
+                }
+	return h;
+        
 	}
-	public String getServerLastName(ServerAccount saccount) {
-		saccount.setLastName("Nabil");
-		String lname = saccount.getLastName();
-		return lname;
+	public String getServerLastName(ServerAccount saccount) throws SQLException{
+            t=w.openconnection().createStatement();
+		ResultSet re=t.executeQuery("select s.Lname from server as s where username='"+saccount.getUserName()+"'" );
+                while(re.next()){
+                e=re.getString("Lname");
+                }
+	return e;
+        
 	}
 }
