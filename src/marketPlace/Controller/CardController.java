76c1db446245e380c4f74c2580e020c10619a49d
NadaSulaiman01 <85398738+NadaSulaiman01@users.noreply.github.com>
2022-07-02 09:04:22 +0200

    Update CardController.java

--- src/marketPlace/Controller/CardController.java
+++ src/marketPlace/Controller/CardController.java
@@ -1,18 +1,51 @@
 package marketPlace.Controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import static marketPlace.MyInfo.socket;
+
 public class CardController {
 	// given client id and product id, retrieve the quantity of
 	// that product in the cart of that client
-	public int getProductQuantityInCart(String clientUserName, int productId) {
+	public int getProductQuantityInCart(String clientUserName, int productId) throws IOException {
 		// this is just a stub
-		return 0;
+		//return 0;
+                 
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                String request = "getproductquantity";
+                String id = String.valueOf(productId);
+                String data = request + ":" + clientUserName + ":" + productId;
+                System.out.println(data);
+                out.println(data);
+                String serverResponse = input.readLine();
+                System.out.println("serverResponse is:" + serverResponse );
+                return Integer.parseInt(serverResponse);
 	}
 	
-	public void addOneToProductQuantityInCart(String clientUserName, int productId) {
+	public void addOneToProductQuantityInCart(String clientUserName, int productId) throws IOException {
 		// ProductQuantityInCart ++;
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                String request = "plusone";
+                String id = String.valueOf(productId);
+                String data = request + ":" + clientUserName + ":" + id;
+                System.out.println(data);
+                out.println(data);
 	}
 	
-	public void minusOneFromProductQuantityInCart(String clientUserName, int productId){
-		// ProductQuantityInCart --;
+	public void minusOneFromProductQuantityInCart(String clientUserName, int productId) throws IOException{
+		 
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                String request = "minusone";
+                String id = String.valueOf(productId);
+                String data = request + ":" + clientUserName + ":" + id;
+                System.out.println(data);
+                out.println(data);
+                         
+                
+           
 	}
-}
